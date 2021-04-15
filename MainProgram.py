d60dc6549b4eaa3061ca6b3156bdd16c79454ecc
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2021-04-14 22:32:27 -0300

    Todo for tomorrow
    Todo for tomorrow

---
+++ MainProgram.py
@@ -0,0 +1,4 @@
+#### Meta amanha:
+#### Ler o arquivo Modules.csv e armazenar todos os registros em uma lista de modulos no main program.
+#### Escrever o Menu
+#### Fazer todas as ações do item i. Add Module
